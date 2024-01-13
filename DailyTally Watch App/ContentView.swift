31fc80ea531a8cf0215625af4b28d64ca17da0ed
andrewbloese-00 <andrewbloese@gmail.com>
2024-01-13 15:43:58 -0700

    Initial Commit

---
+++ DailyTally Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DailyTally Watch App
+//
+//  Created by Andrew Bloese on 1/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DailyTally Watch App/DailyTallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyTallyApp.swift
+//  DailyTally Watch App
+//
+//  Created by Andrew Bloese on 1/13/24.
+//
+
+import SwiftUI
+
+@main
+struct DailyTally_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
